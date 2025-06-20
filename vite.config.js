c8201be6ad2d25d8bd5df6b174d435de1076d333
JullienGault <jullien.gault@orange-store.com>
2025-06-20 12:26:56 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    
+    // Configuration pour copier le worker PDF
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'node_modules/pdfjs-dist/build/pdf.worker.min.js',
+          dest: '.' // Copie le fichier à la racine du dossier de build
+        }
+      ]
+    })
+  ],
 })
