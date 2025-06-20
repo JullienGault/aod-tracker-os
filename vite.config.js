7137c1b29c21189f0e5ed1341c7ca065b071c740
JullienGault <jullien.gault@orange-store.com>
2025-06-20 14:03:22 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  // ON AJOUTE CETTE SECTION 'build'
-  build: {
-    // On cible des navigateurs plus modernes qui comprennent le "top-level await"
-    target: 'es2022' 
-  },
   plugins: [react()],
 })
